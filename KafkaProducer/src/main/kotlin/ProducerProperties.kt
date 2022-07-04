1db082135bf01ffc855d10caa0271f722fed1c8f
pneff93 <pneff@confluent.io>
2022-07-04 13:50:46 +0200

    feat(): implement svc for CFK

--- KafkaProducer/src/main/kotlin/ProducerProperties.kt
+++ KafkaProducer/src/main/kotlin/ProducerProperties.kt
@@ -8,8 +8,8 @@ class ProducerProperties {
         val settings = Properties()
         settings.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
         settings.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroSerializer")
-        settings.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092")
-        settings.setProperty("schema.registry.url", "http://schema-registry:8081")
+        settings.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
+        settings.setProperty("schema.registry.url", "http://schemaregistry.confluent.svc.cluster.local:8081")
 
         return settings
     }
--- KafkaStreams/src/main/kotlin/StreamProperties.kt
+++ KafkaStreams/src/main/kotlin/StreamProperties.kt
@@ -6,10 +6,10 @@ class StreamProperties {
     fun configureProperties(): Properties {
 
         val settings = Properties()
-        settings.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+        settings.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
         settings.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streamsId")
         settings.setProperty(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
-        settings.setProperty("schema.registry.url", "http://localhost:8081")
+        settings.setProperty("schema.registry.url", "http://schemaregistry.confluent.svc.cluster.local:8081")
 
         return settings
     }
