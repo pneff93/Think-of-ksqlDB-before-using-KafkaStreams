d76e6e91f0d3164480c3e673729ba70f27a63cc3
pneff93 <pneff@confluent.io>
2022-07-19 09:39:16 +0200

    feat(): SSL encryption config clients

--- KafkaProducer/src/main/kotlin/ProducerProperties.kt
+++ KafkaProducer/src/main/kotlin/ProducerProperties.kt
@@ -1,4 +1,6 @@
+import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SslConfigs
 import java.util.*
 
 class ProducerProperties {
@@ -8,8 +10,12 @@ class ProducerProperties {
         val settings = Properties()
         settings.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
         settings.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroSerializer")
-        settings.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092")
+        settings.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19093")
         settings.setProperty("schema.registry.url", "http://schema-registry:8081")
+
+        settings.setProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL")
+        settings.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "secret")
+        settings.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "src/main/resources/kafka.client.truststore.jks")
 
         return settings
     }
--- KafkaStreams/src/main/kotlin/StreamProperties.kt
+++ KafkaStreams/src/main/kotlin/StreamProperties.kt
@@ -1,3 +1,5 @@
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.streams.StreamsConfig
 import java.util.*
 
@@ -6,10 +8,14 @@ class StreamProperties {
     fun configureProperties(): Properties {
 
         val settings = Properties()
-        settings.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+        settings.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19093")
         settings.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streamsId")
         settings.setProperty(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
         settings.setProperty("schema.registry.url", "http://localhost:8081")
+
+        settings.setProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL")
+        settings.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "secret")
+        settings.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "src/main/resources/kafka.client.truststore.jks")
 
         return settings
     }
